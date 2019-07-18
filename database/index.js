14964142992bb294909c5b3e95483a20f8fa11f6
morganl92 <morganlinbox@gmail.com>
2019-07-18 15:49:13 -0700

    modified for heroku deployment

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-let DB_URL = (process.env.NODE_ENV === 'development') ? 'mongodb://localhost/fec-navbar' : `mongodb+srv://morganl:${process.env.DB_PW}@tabl-fec-nav-bar-cluster-hokx3.mongodb.net/test?retryWrites=true&w=majority`
+let DB_URL = (process.env.NODE_ENV === 'development') ? 'mongodb://localhost/fec-navbar' : `mongodb+srv://morganl:${process.env.DB_PW}@tabl-fec-nav-bar-cluster-hokx3.mongodb.net/fec-navbar?retryWrites=true&w=majority`
 
 mongoose.connect(DB_URL, {useNewUrlParser: true})
   .then(() => console.log('Connection to mongoDB is successful!'))
