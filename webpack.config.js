7e2f4207d210bf9870f4810a6e04dbededab708c
Morgan Lin <morganjunlin@gmail.com>
2020-03-07 23:58:58 -0800

    Adds SASS to project

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,21 @@ module.exports = {
         ])
       },
       {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          },
+        ],
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [{
           loader: 'file-loader',
