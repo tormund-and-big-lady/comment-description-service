e36f9da22b95f22f58e6b12b7d000e11763d87c0
Morgan Lin <morganjunlin@gmail.com>
2020-03-11 22:57:57 -0700

    [prog] changes Navbar location

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './Navbar';
 
 const navbar = document.getElementById('navbar');
 navbar.style.margin = '50px';
