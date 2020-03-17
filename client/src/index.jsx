8c24134e1ca9a4d4118c8365a5966c211ac949c8
Morgan Lin <morganjunlin@gmail.com>
2020-03-16 23:46:57 -0700

    [prog] fixes comp margins

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './Navbar';
 
-const navbar = document.getElementById('navbar');
-// navbar.style.margin = '50px';
-
-ReactDOM.render(<Navbar />, navbar)
+ReactDOM.render(<Navbar />, document.getElementById('navbar'))
