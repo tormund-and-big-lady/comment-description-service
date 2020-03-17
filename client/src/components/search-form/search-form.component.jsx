1445024d8a5f426a40d65aaec5b7b1c20c6a933d
Morgan Lin <morganjunlin@gmail.com>
2020-03-16 23:47:59 -0700

    [feat] adds onChange search logic form

--- client/src/components/search-form/search-form.component.jsx
+++ client/src/components/search-form/search-form.component.jsx
@@ -1,28 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import SearchResults from './searchResults';
 
 import './search-form.styles.scss';
 
-const SearchForm = props => (
-  <div className='search-form'>
-    <div className='box'>
-      <div className='box-arrow' />
-    </div>
-    <div className='search-box'>
-      <div className='search-box-item'>
-        <input type='text' placeholder='What can we help you find?'/>
-      </div>
-      <div className='search-box-item'>
-        <div className='item-clear'>
-          <span>Clear</span>
-        </div>
-        <div className='item-clear'>
-          <svg height='10' width='10' className='search-close'>
-            <path d='M2 2l8 8m0-8l-8 8'></path>
-          </svg>
-        </div>
-      </div>
-    </div>
-  </div>
-)
+export default class SearchForm extends Component {
+  constructor(props) {
+    super(props);
 
-export default SearchForm;
+    this.state = {
+      search: '',
+      results: [],
+      resultHoverIdx: 0
+    }
+
+    this.handleSearchResults = this.handleSearchResults.bind(this);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut =  this.handleMouseOut.bind(this);
+  }
+
+  handleSearchResults(e) {
+    if (e.target.value) {
+      this.setState({ search: e.target.value }, () =>
+        axios
+          .get(`/navbar/search/${this.state.search}`)
+          .then(({ data }) => this.setState({ results: data }, () => console.log(this.state)))
+          .catch(err => console.log(err)))
+    } else {
+      this.setState({
+        search: '',
+        results: []
+      })
+    }
+  }
+
+  handleMouseOver(stateName, idx) {
+    // if statement is to catch search result related logic
+    if (stateName === 'resultHoverIdx') {
+      this.setState({ [stateName]: idx })
+    } else {
+      this.setState({ [stateName]: true })
+    }
+  }
+
+  handleMouseOut(idx) {
+    this.setState({ [idx]: false })
+  }
+
+  render() {
+    return (
+      <div className={`search-form ${this.props.search ? 'show' : ''}`}>
+        <div className='box'>
+          <div className='box-arrow' />
+        </div>
+        <div className='search-box'>
+          <div className='search-box-item'>
+            <input type='text' placeholder='What can we help you find?' value={this.state.search} onChange={this.handleSearchResults}/>
+          </div>
+          <div className='search-box-item'>
+            <div className='item-clear'>
+              {
+                this.state.search ? <span onClick={() => this.setState({ search: '' })}>Clear</span> : ''
+              }
+            </div>
+            <div className='item-clear' onClick={() => this.props.handleMouseOver('search')}>
+              <svg height='10' width='10' className='search-close'>
+                <path d='M2 2l8 8m0-8l-8 8'></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+        {/* collective search results conditional render */}
+        <ul className='searchResults'>
+          {this.state.results.map((result, index) => 
+            <SearchResults 
+              key={index} 
+              index={index} 
+              result={result} 
+              search={this.state.search} 
+              handleMouseOver={this.handleMouseOver} 
+            />)
+          }
+        </ul>
+
+        {/* additional assets i.e. images render */}
+        {
+          this.state.results.length ?
+          <div>
+            <div className='featuredResult'>
+              <span>
+                <strong>Featured results for "{this.state.results[this.state.resultHoverIdx].keyword}"</strong>
+              </span>
+            </div>
+            <div className='imgResultDiv'>
+              <ul>
+                {
+                  this.state.results[this.state.resultHoverIdx].products.map((product, index) =>
+                  <li key={index} onMouseEnter={() => this.handleMouseOver(index)} onMouseOut={() => this.handleMouseOut(index)}>
+                    {
+                      (this.state[index] && product.images.length > 1) ? <img className='imgResult' src={product.images[1].imageUrl} /> 
+                      : <img className='imgResult' src={product.images[0].imageUrl} />
+                    }
+                  </li>)
+                }
+              </ul>
+            </div>
+          </div> 
+          : ''
+        }
+      </div>
+    )
+  }
+};
