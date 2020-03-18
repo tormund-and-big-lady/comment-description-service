c91f188e6513e945aa3c2fb2c7eae385f60ef317
Morgan Lin <morganjunlin@gmail.com>
2020-03-17 21:20:05 -0700

    [style] destructures prop

---
+++ client/src/components/search-form/search-results.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './search-results.scss';
+
+const SearchResults = ({ index, result, search, handleMouseOver}) => {
+
+  let searchLen = search.length;
+  let searchIdx = result.keyword.search(search);
+  let searchIdxLen = searchIdx + searchLen;
+
+  return (
+    <li className='result'>
+      <span onMouseEnter={() => handleMouseOver('resultHoverIdx', index)}>
+        {result.keyword.slice(0, searchIdx)}<strong>{result.keyword.slice(searchIdx, searchIdxLen)}</strong>{result.keyword.slice(searchIdxLen)}
+      </span>
+    </li>
+  )
+}
+
+export default SearchResults;
