1fd6d01f2c96aff5002b1cbba8d50eab58c82163
Morgan Lin <morganjunlin@gmail.com>
2020-03-20 17:56:07 -0700

    [feat] allows br bundle to be served

--- server/index.js
+++ server/index.js
@@ -12,11 +12,18 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// router
-app.use('/navbar', router)
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
 
 // serving static client front-end files
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+// router
+app.use('/navbar', router)
 
 // establish port to listen on
 let port = process.env.PORT || 3001;
