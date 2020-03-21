9f6be7b04f900c53de736f276543f57b81667ac0
Morgan Lin <morganjunlin@gmail.com>
2020-03-20 17:56:13 -0700

    [feat] allows br bundle to be served

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
+  req.url = req.url + '.br';
+  res.set('Content-Encoding', 'br');
   res.set('Content-Type', 'text/javascript');
   next();
 });
